21daf00c08c2b9e3b2dd657bcfe43c1fd86d868c
vaishnavimysore <mysorevaishnavi@gmail.com>
2023-05-10 19:40:43 -0700

    Updates related to Session authentication and cookies using passport

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 //jshint esversion:6
 
 //Importing the necesssary packages
-
 require('dotenv').config();
 const ejs = require('ejs');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+//Importing session, passport and passport local mongoose packages for using sessions and authentication for our app
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+//passport-local need not be imported as this package with add it by itself as a plugin
 
 
+//Creates the web application with express
 const app = express();
-
-//
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine','ejs');
+
+//Setting up and initializing session using express
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,           //Used for saving the session even when its not modified
+  saveUninitialized: false, //Any session which is created but not modified can be stored
+  cookie: { secure: false }  //Securing the cookies in the session
+}))
+
+//Initializes passport on every route call
+app.use(passport.initialize());
+//Allows passport to use express-session
+app.use(passport.session());
 
 //Creating a DB for users on the mongoDB server
 mongoose.connect("mongodb://localhost:27017/userdB",{ useNewUrlParser: true,useUnifiedTopology: true });
@@ -27,74 +41,104 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
+//Using passport local mongoose package to salt and hash passwords and save into DB
+userSchema.plugin(passportLocalMongoose);
+
 //Creating the schema model in DB
 const User = mongoose.model("User",userSchema);
 
+
+//Create strategy for storing our user data
+passport.use(User.createStrategy());
+//Allows passport to Create a cookie and add our user identifications data into it
+passport.serializeUser(User.serializeUser());
+//Allows passport to Break the cookie and uses our user identifications data
+passport.deserializeUser(User.deserializeUser());
+
+
 //GET request for home route
 app.get("/",function(req,res){
-//Renders the home.ejs file
-res.render("home");
+  //Renders the home.ejs file
+  res.render("home");
 });
 
 //GET request for login route
 app.get("/login",function(req,res){
-//Renders the login.ejs file
-res.render("login");
+  //Renders the login.ejs file
+  res.render("login");
 });
 
 //GET request fpr register route
 app.get("/register",function(req,res){
-//Renders the register.ejs file
-res.render("register");
+  //Renders the register.ejs file
+  res.render("register");
 });
 
-//POST method for register route
-app.post("/register",function(req,res){
-
-//Using the bcrypt methods for creating salt and hash
-  bcrypt.genSalt(saltRounds, function(err, salt) {
-      bcrypt.hash(req.body.password, salt, function(err, hash) {
-          // Store hash in your password DB.
-          //Using the data entered in the page and storing in the DB by creating a new user
-           const newUser = new User({
-             email: req.body.username,
-             password: hash
-           });
-
-          //Saving the new user data in the DB and checking if there are any error while doing so
-           newUser.save().then(function(){
-             //Renders the secret.ejs file when the save is successful
-             res.render("secrets");
-           }).catch(function(err){
-             //Renders the error when there is a failure
-             res.render(err);
-           });
-      });
-  });
-});
-
-//POST method for the login route
-app.post("/login",function(req,res){
-
-//Storing the data entered in variables
-const email = req.body.username;
-const password = req.body.password;
-
-//Fetching the email ID from the DB and checing with the entered one
-User.findOne({email:email}).then(function(foundUser){
-
-//If the passwords match, then secrest.ejs files is rendered, else the error is rendered
-//Using bycrypt menthod for comparing the password typed and the hashed password
-bcrypt.compare(password, foundUser.password, function(err, result) {
-  // result == true
-  if(result){
-  res.render("secrets");
+app.get("/secrets",function(req,res){
+  // The below line was added so we can't display the "/secrets" page
+  // after we logged out using the "back" button of the browser, which
+  // would normally display the browser cache and thus expose the
+  // "/secrets" page we want to protect. Code taken from this post.
+  res.set(
+    'Cache-Control',
+    'no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0'
+  );
+  //check if the user is already authenticated and render the secrets page
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  }
+  else {
+    res.redirect('/login');
   }
 });
 
-}).catch(function(err){
-  res.render(err);
+//GET request for logout page
+app.get("/logout",function(req,res){
+  //using the passport function to logout(which breaks the cookies data) and redirects to home page
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
+
+
+//POST method for register route
+app.post("/register",function(req,res){
+  //passportLocalMongoose method to register a user to our db
+  User.register({username:req.body.username}, req.body.password, function(err, user) {
+    if (err)
+    {
+      console.log(err);
+      //Incase of an error while register, the user is redirected to the register route
+      res.redirect('/register');
+    }
+    else{
+      //When the user is successfully register, we authenticate the user using passport using local strategy and redirect to secrets route
+      passport.authenticate("local")(req,res,function(){
+        res.redirect('/secrets');
+      });
+    }
+  });
+});
+
+//POST method for the login route which first authenticates(verifies the user against their credentials) the user and then logs the user in
+app.post("/login",passport.authenticate("local",{failureRedirect:'/login'}),function(req,res){
+  //Storing the data entered, in user object
+  const user = new User({
+    username:req.body.username,
+    password : req.body.password
+  });
+  //Passport function to login the user with the "user" object details
+  req.login(user,function(err){
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.redirect('/secrets');
+    }
+  });
 });
 
 //Port/Local server where the web app is hosted on
