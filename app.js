bdc438b9c07bfffc8663a25b71d0d32088651847
vaishnavimysore <mysorevaishnavi@gmail.com>
2023-05-11 20:49:49 -0700

    Adding google and facebook strategies for authentication and login

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 //passport-local need not be imported as this package with add it by itself as a plugin
+//importing passport-google packages for login and authentication
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+//Package to use the findOrCreate function in google and facebook strategies
+const findOrCreate = require('mongoose-findorcreate');
+//importing passport-facebook packages for login and authentication
+const FacebookStrategy = require("passport-facebook");
 
-
-//Creates the web application with express
+//Creates the web application with expresss
 const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -38,23 +43,70 @@ mongoose.connect("mongodb://localhost:27017/userdB",{ useNewUrlParser: true,useU
 //Creating a schema for our user Data
 const userSchema = new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String,
+  facebookId:String
 });
 
 //Using passport local mongoose package to salt and hash passwords and save into DB
 userSchema.plugin(passportLocalMongoose);
-
+//Using findOrCreate functin of its package to use in strategies
+userSchema.plugin(findOrCreate);
 //Creating the schema model in DB
 const User = mongoose.model("User",userSchema);
 
 
 //Create strategy for storing our user data
 passport.use(User.createStrategy());
-//Allows passport to Create a cookie and add our user identifications data into it
-passport.serializeUser(User.serializeUser());
-//Allows passport to Break the cookie and uses our user identifications data
-passport.deserializeUser(User.deserializeUser());
 
+//Allows passport to Create a cookie and add our user identifications data into it
+//Common serialize function for all passport strategies
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
+
+//Allows passport to Break the cookie and uses our user identifications data
+//Common deserialize function for all passport strategies
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+//GOOGLE Strategy to Login
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    uerProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+//FACEBOOK Strategy to Login
+passport.use(new FacebookStrategy({
+    clientID: process.env.FB_CLIENT_ID,
+    clientSecret: process.env.FB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //GET request for home route
 app.get("/",function(req,res){
@@ -74,6 +126,29 @@ app.get("/register",function(req,res){
   res.render("register");
 });
 
+//Google Authentication
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
+
+//Facebook Authentication
+  app.get('/auth/facebook',
+    passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect secrets.
+      res.redirect('/secrets');
+    });
+
+//GET request for secrets route
 app.get("/secrets",function(req,res){
   // The below line was added so we can't display the "/secrets" page
   // after we logged out using the "back" button of the browser, which
@@ -129,6 +204,7 @@ app.post("/login",passport.authenticate("local",{failureRedirect:'/login'}),func
   const user = new User({
     username:req.body.username,
     password : req.body.password
+
   });
   //Passport function to login the user with the "user" object details
   req.login(user,function(err){
